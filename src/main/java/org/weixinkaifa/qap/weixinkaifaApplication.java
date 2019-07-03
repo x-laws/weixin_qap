1bde6b2ae3888b625a0960a2c2dbdc4307a7bf5a
admin <admin@DESKTOP-O988L0V>
2019-07-03 21:57:44 +0800

    第二次提交Spring Boot项目模板

--- src/main/java/org/weixinkaifa/qap/weixinkaifaApplication.java
+++ src/main/java/org/weixinkaifa/qap/weixinkaifaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 微信项目Application {
+public class weixinkaifaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(微信项目Application.class, args);
+		SpringApplication.run(weixinkaifaApplication.class, args);
 	}
 
 }
--- src/test/java/org/weixinkaifa/qap/weixinkaifaApplicationTests.java
+++ src/test/java/org/weixinkaifa/qap/weixinkaifaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class 微信项目ApplicationTests {
+public class weixinkaifaApplicationTests {
 
 	@Test
 	public void contextLoads() {
