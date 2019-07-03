6677d1fe18e772bb34e41c324e499241fe00a249
admin <admin@DESKTOP-O988L0V>
2019-07-03 21:33:33 +0800

    初次提交Spring Boo的项目模板

---
+++ src/main/java/org/weixinkaifa/qap/微信项目Application.java
@@ -0,0 +1,13 @@
+package org.weixinkaifa.qap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 微信项目Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(微信项目Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/weixinkaifa/qap/微信项目ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.weixinkaifa.qap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class 微信项目ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
