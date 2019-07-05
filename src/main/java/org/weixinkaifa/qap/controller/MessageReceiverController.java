1b54c2344d012dc2a76325d7464e65829cd4c478
admin <admin@DESKTOP-O988L0V>
2019-07-05 23:13:20 +0800

    新增微信接收消息，微信接口

--- src/main/java/org/weixinkaifa/qap/controller/MessageReceiverController.java
+++ src/main/java/org/weixinkaifa/qap/controller/MessageReceiverController.java
@@ -1,5 +1,7 @@
 package org.weixinkaifa.qap.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +10,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/qap/message/receiver")
 public class MessageReceiverController {
-		@GetMapping
-		public String echo(//
+	
+private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class)
+	
+	@Autowired
+	private XmlMapper xmlMapper;
+	@Autowired
+	private RedisTemplate<String, ? extends InMessage> inMessageTemplate;
+	
+	@GetMapping
+	public String echo(//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestParam("echostr") String echostr//
+	) {
+		return echostr;
+	}
+		
+		@PostMapping
+		// @RequestBody注解表示把请求内容获取出来，并且转换为String传入给xml参数。
+		public String onMessage(//
 				@RequestParam("signature") String signature, //
 				@RequestParam("timestamp") String timestamp, //
 				@RequestParam("nonce") String nonce, //
-				@RequestParam("echostr") String echostr//
-		) {
-			return echostr;
+				@RequestBody String xml) throws JsonParseException, JsonMappingException, IOException {
+			// 收到消息
+			// {}是占位符，第一个{}会把第二个参数的值自动填入
+			// LOG.trace必须要求日志记录器的配置为trace级别才能输出
+			LOG.trace("收到的消息原文：\n{}\n------------------------------", xml);
+			// 转换消息
 
+
+			InMessage inMessage = convert(xml);
+
+			if (inMessage == null) {
+				LOG.error("消息无法转换！原文：\n{}\n", xml);
+				// 消息无法转换
+				return "success";
+			}
+
+			LOG.debug("转换后的消息对象\n{}\n", inMessage);
+
+			String channel = "lzj" + inMessage.getMsgType();
+			// 把消息丢入队列
+			
+			inMessageTemplate.convertAndSend(channel, inMessage);
+
+			// 消费队列中的消息
+			// 产生客服消息
+
+			return "success";
 		}
-}
+		private InMessage convert(String xml) throws JsonParseException, JsonMappingException, IOException {
+			Class<? extends InMessage> c = MessageConvertHelper.getClass(xml);
+			InMessage msg = xmlMapper.readValue(xml, c);
+			return msg;
+		}
+	}
