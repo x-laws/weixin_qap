d97c3e4ff602248d00548619621218c5972d2d06
admin <admin@DESKTOP-O988L0V>
2019-07-07 19:43:07 +0800

    最新2019/7/7晚上19：43

--- src/main/java/org/weixinkaifa/qap/controller/MessageReceiverController.java
+++ src/main/java/org/weixinkaifa/qap/controller/MessageReceiverController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.weixinkaifa.qap.dumain.InMessage;
 import org.weixinkaifa.qap.service.MessageConvertHelper;
