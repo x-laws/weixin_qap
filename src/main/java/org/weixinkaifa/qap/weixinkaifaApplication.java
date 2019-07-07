0341b60c34c8363af25282234372c64f3632a72a
admin <admin@DESKTOP-O988L0V>
2019-07-07 21:38:34 +0800

    最新代码2019/7/7晚上21：38

--- src/main/java/org/weixinkaifa/qap/weixinkaifaApplication.java
+++ src/main/java/org/weixinkaifa/qap/weixinkaifaApplication.java
@@ -2,13 +2,14 @@ package org.weixinkaifa.qap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @SpringBootApplication
 public class weixinkaifaApplication {
 	
-	
+	@Bean
 	public XmlMapper xmlMapper() {
 		XmlMapper mapper  = new XmlMapper();
 		return mapper;
